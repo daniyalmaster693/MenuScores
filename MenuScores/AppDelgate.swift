43fcda717588713243b75692a3a6e7d997932498
Daniyal Master <daniyalmaster693@gmail.com>
2026-07-06 14:37:37 -0400

    Added appstorage item for onboarding

--- MenuScores/AppDelgate.swift
+++ MenuScores/AppDelgate.swift
@@ -16,33 +16,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         let hasShownTour = UserDefaults.standard.bool(forKey: "hasShownTour")
 
-//        if !hasShownTour {
-        NSApp.setActivationPolicy(.regular)
-        NSApp.activate(ignoringOtherApps: true)
+        if !hasShownTour {
+            NSApp.setActivationPolicy(.regular)
+            NSApp.activate(ignoringOtherApps: true)
 
-        tour.present(
-            pages: [
-                TourPage(imageName: "tour-welcome", title: "Welcome to MenuScores", description: "Live scores - Right From Your Notch"),
-                TourPage(imageName: "tour-compact", title: "Sleek and Minimal", description: "Stay effortlessly updated."),
-                TourPage(imageName: "tour-views", title: "Know the Matchup", description: "View relveant details before games to stay up to date."),
-                TourPage(imageName: "tour-favorites", title: "Track your Favorites", description: "Choose your favorite team and we'll handle the pinning for you."),
-                TourPage(imageName: "tour-leagues", title: "48+ Leagues to Choose From", description: "Track over 48 different leagues across 12 different sports."),
+            tour.present(
+                pages: [
+                    TourPage(imageName: "tour-welcome", title: "Welcome to MenuScores", description: "Live scores - Right From Your Notch"),
+                    TourPage(imageName: "tour-compact", title: "Sleek and Minimal", description: "Stay effortlessly updated."),
+                    TourPage(imageName: "tour-views", title: "Know the Matchup", description: "View relveant details before games to stay up to date."),
+                    TourPage(imageName: "tour-favorites", title: "Track your Favorites", description: "Choose your favorite team and we'll handle the pinning for you."),
+                    TourPage(imageName: "tour-leagues", title: "48+ Leagues to Choose From", description: "Track over 48 different leagues across 12 different sports."),
 
-            ],
-            width: 660,
-            continueButtonTitle: "Continue",
-            finishButtonTitle: "Get Started",
-            onFinish: {
-                UserDefaults.standard.set(true, forKey: "hasShownTour")
-                NSApp.setActivationPolicy(.accessory)
-            },
-            onClose: {
-                UserDefaults.standard.set(true, forKey: "hasShownTour")
-                NSApp.setActivationPolicy(.accessory)
-            }
-        )
-//        } else {
-//            NSApp.setActivationPolicy(.accessory)
-//        }
+                ],
+                width: 660,
+                continueButtonTitle: "Continue",
+                finishButtonTitle: "Get Started",
+                onFinish: {
+                    UserDefaults.standard.set(true, forKey: "hasShownTour")
+                    NSApp.setActivationPolicy(.accessory)
+                },
+                onClose: {
+                    UserDefaults.standard.set(true, forKey: "hasShownTour")
+                    NSApp.setActivationPolicy(.accessory)
+                }
+            )
+        } else {
+            NSApp.setActivationPolicy(.accessory)
+        }
     }
 }
