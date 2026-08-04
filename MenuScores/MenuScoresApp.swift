f3e886cf4e381db259fd9fe5889322e7d67922e2
Daniyal Master <daniyalmaster693@gmail.com>
2026-08-04 11:10:07 -0400

    Removed extra variable declrations in main app.swift file

--- MenuScores/MenuScoresApp.swift
+++ MenuScores/MenuScoresApp.swift
@@ -21,25 +21,6 @@ struct MenuScoresApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     private let updateManager = UpdateManager()
 
-    // Refresh Interval Settings
-
-    @AppStorage("refreshInterval") private var selectedOption = "15 seconds"
-
-    private var refreshInterval: TimeInterval {
-        switch selectedOption {
-        case "10 seconds": return 10
-        case "15 seconds": return 15
-        case "20 seconds": return 20
-        case "30 seconds": return 30
-        case "40 seconds": return 40
-        case "50 seconds": return 50
-        case "1 minute": return 60
-        case "2 minutes": return 120
-        case "5 minutes": return 300
-        default: return 15
-        }
-    }
-
     // Toggled League Settings
 
     @AppStorage("enableNHL") private var enableNHL = true
@@ -108,11 +89,6 @@ struct MenuScoresApp: App {
     @AppStorage("enableOWIHC") private var enableOWIHC = false
     @AppStorage("enableOMB") private var enableOMB = false
     @AppStorage("enableOWB") private var enableOWB = false
-
-    // Notification Settings
-
-    @AppStorage("notiGameStart") private var notiGameStart = false
-    @AppStorage("notiGameComplete") private var notiGameComplete = false
 
     // Title State Settings
 
