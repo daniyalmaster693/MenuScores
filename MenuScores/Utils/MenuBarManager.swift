6c732a013b7022a984472edda2cff690df6be7e1
Daniyal Master <daniyalmaster693@gmail.com>
2026-08-07 11:20:07 -0400

    Added new menubar view files

---
+++ MenuScores/Utils/MenuBarManager.swift
@@ -0,0 +1,10 @@
+//
+//  MenuBarManager.swift
+//  MenuScores
+//
+//  Created by Daniyal Master on 2026-08-07.
+//
+
+import SwiftUI
+
+class MenuBarManager {}
---
+++ MenuScores/Views/MenuBar Views/Compact.swift
@@ -0,0 +1,12 @@
+//
+//  Compact.swift
+//  MenuScores
+//
+//  Created by Daniyal Master on 2026-08-07.
+//
+
+import SwiftUI
+
+struct CompactMenuBar {
+//    var body: some Scene {}
+}
